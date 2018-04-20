72e7e0ca7841d34fa4fd4e4a015da67d863e5e3b
sakura-fly <sourcesakura@sina.com>
2018-04-20 21:13:22 +0800

    期刊书籍表格

--- app/js/admin.js
+++ app/js/admin.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $("#qk").click(function(event) {
-    	$("#ucontent").load("bookman.html");
+    	$("#ucontent").load("qikanadmin.html");
     });
     $("#books").click(function (e) {
-        $("#ucontent").load("userman.html");
+        $("#ucontent").load("bookman.html");
     })
 });
