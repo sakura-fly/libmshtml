fc9be2f1801fb537016b31f79f1eea0f9d01b408
sakura-fly <sourcesakura@sina.com>
2018-04-20 20:37:29 +0800

    增加侧边菜单

--- app/js/admin.js
+++ app/js/admin.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $("#qk").click(function(event) {
     	$("#ucontent").load("bookman.html");
     });
+    $("#books").click(function (e) {
+        $("#ucontent").load("userman.html");
+    })
 });
